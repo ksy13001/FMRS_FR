39df7d045e9256f49e4189193de16f748b8a407d
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-07-21 14:30:57 +0900

    tailwind 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
+import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google"
 import "./globals.css"
 import { AuthProvider } from "@/components/auth-provider"
 
@@ -18,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   )
